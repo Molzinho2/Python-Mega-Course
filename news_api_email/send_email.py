b86ee48852ed2580d669f8b437097b80ff404403
Igor Rafael Mol Rodrigues <igor.mol2013@gmail.com>
2026-01-21 10:15:55 -0300

    modified:   news_api_email/send_email.py

--- news_api_email/send_email.py
+++ news_api_email/send_email.py
@@ -4,10 +4,10 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 587 
     
-    username = "igor.mol2013@gmail.com"
-    password = "aveprugsfihwahhv"  # Use an app password for Gmail
+    username = "YOUR EMAIL"
+    password = "YOUR PASSWORD"  # Use an app password for Gmail
     
-    receiver = "igor-mol@hotmail.com"
+    receiver = "EMAIL RECEIVER"
     ##context = ssl.create_default_context()
     
     with smtplib.SMTP(host, port) as server:
